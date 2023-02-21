1164071d961ed5d464b4f5ca3e7dfbdb03182e4f
Roman Kuzmenko <openvmp@proton.me>
2023-02-20 19:06:40 -0800

    Throttled the number of CPUs used during build. Succesfull builds may get a little slower. But failed builds fail much faster.

--- deployment/ansible/scripts/build_rpi.sh
+++ deployment/ansible/scripts/build_rpi.sh
@@ -2,5 +2,14 @@
 
 source /opt/ros/humble/setup.bash
 #[ -f ./install/local_setup.bash ] && source ./install/local_setup.bash
+
+# --parallel-workers 2: colcon spawns as many workers as there are CPUs, but
+#                       CPU performance is not the bottleneck on Raspberry PI.
+#                       SD card I/O is. Using two workers so that one is always
+#                       waiting on I/O and the other one is busy preparing
+#                       the next chunk of data for I/O.
+#
+# --packages-skip openvmp_hardware_simulation_gazebo: no gazebo dependencies
 COLCON_HOME=$(pwd) colcon build \
+  --parallel-workers 2 \
   --packages-skip openvmp_hardware_simulation_gazebo
